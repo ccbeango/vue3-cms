c32953805f59d81bc9dfbffc8c466090562df15a
ccbean <liuyh940@gmail.com>
2022-07-25 22:20:05 +0800

    init(other): 🎉 项目初始化

---
+++ commitlint.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 80],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'test',
+        'init',
+        'docs',
+        'revert',
+        'refactor',
+        'style',
+        'ci',
+        'perf'
+      ]
+    ],
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-empty': [1, 'always'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always']
+  }
+}
